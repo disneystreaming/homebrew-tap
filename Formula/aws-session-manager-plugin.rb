1f1bbe2c1accfc8a82b75886a1d93d397d298482
Justin Garrison <justinleegarrison@gmail.com>
2020-03-25 17:40:41 -0700

    Fix sha256

--- Formula/aws-session-manager-plugin.rb
+++ Formula/aws-session-manager-plugin.rb
@@ -5,20 +5,19 @@ class AwsSessionManagerPlugin < Formula
 
   if OS.mac?
     url "https://s3.amazonaws.com/session-manager-downloads/plugin/#{version}/mac/sessionmanager-bundle.zip"
+    sha256 "d9b558193370b2ecc0ddba001b6ee974b14b60d4d247851706e26a9811f15349"
 
     def install
       bin.install "bin/session-manager-plugin"
       prefix.install %w[LICENSE VERSION]
     end
 
-  sha256 "d9b558193370b2ecc0ddba001b6ee974b14b60d4d247851706e26a9811f15349"
-
   # Linux Install extracts the deb file
   elsif OS.linux?
     if Hardware::CPU.intel?
       if Hardware::CPU.is_64_bit?
         url "https://s3.amazonaws.com/session-manager-downloads/plugin/#{version}/ubuntu_64bit/session-manager-plugin.deb"
-        sum256 "342602b05552268966bcac2ec7698447e1202eb8e3d2943066dd9954697604ef"
+        sha256 "342602b05552268966bcac2ec7698447e1202eb8e3d2943066dd9954697604ef"
       elsif Hardware::CPU.is_32_bit?
         url "https://s3.amazonaws.com/session-manager-downloads/plugin/#{version}/ubuntu_32bit/session-manager-plugin.deb"
       end
