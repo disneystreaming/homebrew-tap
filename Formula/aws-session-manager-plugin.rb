fd0189221d0d583b676cf7bfccf1d8d0c6f3ba9a
Justin Garrison <justinleegarrison@gmail.com>
2020-03-25 17:39:03 -0700

    Fix metafiles

--- Formula/aws-session-manager-plugin.rb
+++ Formula/aws-session-manager-plugin.rb
@@ -12,10 +12,13 @@ class AwsSessionManagerPlugin < Formula
     end
 
   sha256 "d9b558193370b2ecc0ddba001b6ee974b14b60d4d247851706e26a9811f15349"
+
+  # Linux Install extracts the deb file
   elsif OS.linux?
     if Hardware::CPU.intel?
       if Hardware::CPU.is_64_bit?
         url "https://s3.amazonaws.com/session-manager-downloads/plugin/#{version}/ubuntu_64bit/session-manager-plugin.deb"
+        sum256 "342602b05552268966bcac2ec7698447e1202eb8e3d2943066dd9954697604ef"
       elsif Hardware::CPU.is_32_bit?
         url "https://s3.amazonaws.com/session-manager-downloads/plugin/#{version}/ubuntu_32bit/session-manager-plugin.deb"
       end
@@ -24,7 +27,7 @@ class AwsSessionManagerPlugin < Formula
         system "ar", "x", "session-manager-plugin.deb"
         system "tar", "xf", "data.tar.gz"
         bin.install "usr/local/sessionmanagerplugin/bin/session-manager-plugin"
-        prefix.install %w[LICENSE VERSION]
+        prefix.install_metafiles
       end
     end
   end
