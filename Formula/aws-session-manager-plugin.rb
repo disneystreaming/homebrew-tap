96afecb6f0447509ea83ccf85888f690b3ed6b22
Justin Garrison <justin@linux.com>
2020-07-21 18:12:28 -0700

    Update session manager plugin to 1.1.61.0 add arm
    I edited in GitHub web UI. Hope the linting is right.

--- Formula/aws-session-manager-plugin.rb
+++ Formula/aws-session-manager-plugin.rb
@@ -1,11 +1,11 @@
 class AwsSessionManagerPlugin < Formula
   desc "Official Amazon AWS session manager plugin"
   homepage "https://docs.aws.amazon.com/systems-manager/latest/userguide/session-manager-working-with-install-plugin.html"
-  version "1.1.54.0"
+  version "1.1.61.0"
 
   if OS.mac?
     url "https://s3.amazonaws.com/session-manager-downloads/plugin/#{version}/mac/sessionmanager-bundle.zip"
-    sha256 "d9b558193370b2ecc0ddba001b6ee974b14b60d4d247851706e26a9811f15349"
+    sha256 "426615b12b2d7728504ee7896c60fa330534314d6cb64ee8cb3ea0488e7d66e4"
 
     def install
       bin.install "bin/session-manager-plugin"
@@ -17,10 +17,15 @@ class AwsSessionManagerPlugin < Formula
     if Hardware::CPU.intel?
       if Hardware::CPU.is_64_bit?
         url "https://s3.amazonaws.com/session-manager-downloads/plugin/#{version}/ubuntu_64bit/session-manager-plugin.deb"
-        sha256 "342602b05552268966bcac2ec7698447e1202eb8e3d2943066dd9954697604ef"
+        sha256 "d4d578a64210165ec434d658212304a968acb2efa49074868552427e738ea97c"
       elsif Hardware::CPU.is_32_bit?
         url "https://s3.amazonaws.com/session-manager-downloads/plugin/#{version}/ubuntu_32bit/session-manager-plugin.deb"
+        sha256 "1aca606a9d30cf562ef9b1d6bfb2f69c785ad23dd63a34bebc8a103402dcc8c2"
       end
+    elsif Hardware::CPU.arm?
+      url "https://s3.amazonaws.com/session-manager-downloads/plugin/#{version}/ubuntu_arm64/session-manager-plugin.deb"
+      sha256 "04a96988bd45ff12486305f337c93107efdc2be9653dc9ce84770ffc6ab07052"
+    end
 
       def install
         system "ar", "x", "session-manager-plugin.deb"
@@ -28,7 +33,6 @@ class AwsSessionManagerPlugin < Formula
         bin.install "usr/local/sessionmanagerplugin/bin/session-manager-plugin"
         prefix.install_metafiles
       end
-    end
   end
 
   depends_on "awscli"
