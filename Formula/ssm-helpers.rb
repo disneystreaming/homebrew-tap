b48023ebed57314c526fc166f50e8d1ad86a62a4
goreleaserbot <goreleaser@carlosbecker.com>
2021-10-27 12:57:40 -0700

    Brew formula update for ssm-helpers version v1.1.1

--- Formula/ssm-helpers.rb
+++ Formula/ssm-helpers.rb
@@ -5,28 +5,32 @@
 class SsmHelpers < Formula
   desc "Help manage systems with AWS Systems Manager with management helpers."
   homepage "https://github.com/disneystreaming/ssm-helpers"
-  version "1.1.0"
-  bottle :unneeded
+  version "1.1.1"
 
   on_macos do
     if Hardware::CPU.intel?
-      url "https://github.com/disneystreaming/ssm-helpers/releases/download/v1.1.0/ssm-helpers_1.1.0_Darwin_x86_64.tar.gz"
-      sha256 "2b4c69dd06625bad43d03d30fdfaf10e6a8a6cd20319398383a96af71a71db6a"
+      url "https://github.com/disneystreaming/ssm-helpers/releases/download/v1.1.1/ssm-helpers_1.1.1_Darwin_x86_64.tar.gz"
+      sha256 "4bad096d8820ea7acc1076dda34fee82022abf52f8f04e1e1ffcabb410469001"
+
+      def install
+        bin.install "ssm"
+        bin.install_symlink  bin/"ssm" => "ssm-helpers"
+      end
     end
   end
 
   on_linux do
     if Hardware::CPU.intel?
-      url "https://github.com/disneystreaming/ssm-helpers/releases/download/v1.1.0/ssm-helpers_1.1.0_Linux_x86_64.tar.gz"
-      sha256 "14d2669378556d8a6df773d62c0cee2ff9a759aa783abb77bf8fc56e237fae95"
+      url "https://github.com/disneystreaming/ssm-helpers/releases/download/v1.1.1/ssm-helpers_1.1.1_Linux_x86_64.tar.gz"
+      sha256 "40424036a02b09db974e0e3137a6eda2dffb6823a08a1a42c7b50a06dcd79bb0"
+
+      def install
+        bin.install "ssm"
+        bin.install_symlink  bin/"ssm" => "ssm-helpers"
+      end
     end
   end
 
   depends_on "awscli"
   depends_on "disneystreaming/tap/aws-session-manager-plugin"
-
-  def install
-    bin.install "ssm"
-    bin.install_symlink  bin/"ssm" => "ssm-helpers"
-  end
 end
